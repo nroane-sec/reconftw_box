57e123b4e58bdd0e38e1de6ac6620f35e2d8d964
nroane-sec <105808274+nroane-sec@users.noreply.github.com>
2023-05-08 14:25:37 -0400

    Update check_active_scans.sh
    Added a check to see if the scan you are trying to queue is already queued up, if not: launches your scan

--- check_active_scans.sh
+++ check_active_scans.sh
@@ -9,38 +9,38 @@ echo "Your desired scan is: $scan"
 function check_active_scans(){
 	ps aux | grep -v grep | grep reconftw > /dev/null #redir output to hide it
 	
-	check="$?" #need to assign this variable right away so it captures the exit status of the ps above
+	check="$?" # need to assign this variable right away so it captures the exit status of the ps above
 
-	echo "$check"
+	#echo "$check"
 
 	if [[ "$check" -eq "0" ]]; then
-		echo "THERE IS ALREADY AND INSTANCE OF RECONFTW IN PROGRESS..."
+		echo "THERE IS ALREADY AND INSTANCE OF RECONFTW IN PROGRESS...\n"
 		echo "Would you like to add your scan to the queue? Y/N"
 		#sleep 5
 		read queue_scan
 		
 		if [[ $queue_scan == "Y" ]]; then
-			# TODO - need to check if that scan is already queued so people don't clog it up with duplicates
-
 			
-			echo "Adding your scan to the queue..."
-			echo "$scan" >> queued_scans.rftw
+
+			grep -n "$scan" queued_scans.rftw 		# This checks the queued_scans.rftw file to see if the desired scan is already queued up
+			
+			if [[ $? -eq "0" ]]; then
+				echo "This scan is already queued, sorry"
+			elif [[ $? -eq "1" ]]; then
+				echo "Adding your scan to the queue..."
+				echo "$scan" >> queued_scans.rftw
+			fi
 
 		elif [[ $queue_scan == "N" ]]; then
-			echo "Ok, we won't add you scan"
+			echo "Ok, we won't add your scan"
 		fi
-		# TODO Add the desired scan to a queue file?
+
 
 	elif [[ "$check" -eq 1 ]]; then
-		#statements
+
 		echo "There are no active scans running, you may proceed... with caution"
+		$scan 	# This launches the desired scan
 	fi
 }
 
 check_active_scans
-
-
-# TODO
-# What do we do next?
-# DONE -push the desired scan into a file to be called upon when the initial scan has finished?
-# - Figure out how to pop the top value out of the queue file and then start that scan
